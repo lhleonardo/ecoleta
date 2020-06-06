dc27fca4f26c42a55439de3ee736583af9de8727
Leonardo Braz <lhleonardo05@gmail.com>
2020-06-06 12:00:28 -0300

    Extrai o banco de dados para a raiz do projeto

--- knexfile.ts
+++ knexfile.ts
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
   client: "sqlite3",
   connection: {
-    filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+    filename: path.resolve(__dirname, "database.sqlite"),
   },
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "migrations"),
