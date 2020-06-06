049d35afbd83e30881b662964228c7b20427be7d
Leonardo Braz <lhleonardo05@gmail.com>
2020-06-06 00:58:20 -0300

    Cria primeiras seeds para items

---
+++ src/database/seeds/create_items.ts
@@ -0,0 +1,32 @@
+import Knex from "knex";
+
+const INITIAL_DATA = [
+  {
+    title: "Lâmpadas",
+    image: "lampada.svg",
+  },
+  {
+    title: "Pilhas e Baterias",
+    image: "baterias.svg",
+  },
+  {
+    title: "Papéis e Papelão",
+    image: "papeis-papelao.svg",
+  },
+  {
+    title: "Resíduos Eletrônicos",
+    image: "eletronicos.svg",
+  },
+  {
+    title: "Resíduos Orgânicos",
+    image: "organicos.svg",
+  },
+  {
+    title: "Óleo de Cozinha",
+    image: "oleo.svg",
+  },
+];
+
+export async function seed(knex: Knex) {
+  await knex("items").insert(INITIAL_DATA);
+}
